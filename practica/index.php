3a4ec6eabbca7c70970b1c3aaa1012b2f445f5a3
jovaslink <jovaslink@hotmail.com>
2016-06-08 22:57:16 -0500

    mvc completo

---
+++ practica/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once("config.php");
+if(!empty($_SESSION["modulo_8"]))
+	{
+		$accionpredefinida="home";
+		if(!empty($_GET["accion"]))
+			{
+				$accion=$_GET["accion"];
+			}
+		else
+			{
+				$accion=$accionpredefinida;
+			}
+		if(is_file("controller/".$accion."Controller.php"))
+			{
+				require_once("controller/".$accion."Controller.php");
+			}
+		else
+			{
+				require_once("controller/errorController.php");
+			}		
+	}
+else
+	{
+		require_once("controller/indexController.php")
+	}	
+
+?>
---
+++ script/funciones.js
@@ -0,0 +1,7 @@
+function eliminar(url)
+{
+	if (confirm("Realmente desea Usted eliminar este registro?"))
+	{
+		window.location=url;
+	}
+}
